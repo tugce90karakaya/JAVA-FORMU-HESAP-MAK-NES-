ce77b14bf853b9c37cc24605b11407a1a12a8a0a
Tuğçe Karakaya <tugcekarakaya90@gmail.com>
2022-09-12 10:38:32 +0300

    Java formu hesap makinesi

---
+++ src/main/java/com/mycompany/hesapmakinesi/HesapMakinesi.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.hesapmakinesi;
+
+/**
+ *
+ * @author ASUS PC
+ */
+public class HesapMakinesi {
+
+    public static void main(String[] args) {
+        Makine hesapMakinesi = new Makine();
+        hesapMakinesi.setVisible(true);
+    }
+}
